6017dc39b71708b65ef55fdebef44586ede92d9f
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-04-16 15:49:45 +0100

    config.SAMPLE.php now shows required variables
    Fixes #2

--- inc/config.SAMPLE.php
+++ inc/config.SAMPLE.php
@@ -1,18 +1,42 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $db_host		=	"localhost";
-$db_name		=	"DATABASE";	
-$db_username	=	"USERNAME";
-$db_password	=	"PASSWORD";
+$db_name		=	"database-name";	
+$db_username	=	"database-username";
+$db_password	=	"database-password";
 
-$colour_gas_year1 = "blue";
-$colour_gas_year2 = "purple";
-$colour_gas_year3 = "green";
-$colour_electric_year1 = "red";
-$colour_electric_year2 = "orange";
-$colour_electric_year3 = "yellow";
+define("LDAP_SERVER", "ldap-server");
+define("LDAP_BASE_DN", "DC=some,DC=ldap,DC=base,DC=dn");
+define("LDAP_ACCOUNT_SUFFIX", "@some.ldap.account.suffix");
 
+define("BUDGET_STARTDATE", "2019-08-01");
+define("BUDGET_ENDDATE", "2020-07-31");
+
+function daysIntoBudget() {
+	$timediff = time() - strtotime(BUDGET_STARTDATE);
+	$datediff = round($timediff / (60 * 60 * 24));
+	
+	return ($datediff);
+}
+
+function percentageIntoBudget() {
+	$totalDaysThisYear = cal_days_in_year(date('Y'));
+	$daysIntoBudget = daysIntoBudget();
+	
+	$percentagediff = round(($daysIntoBudget / $totalDaysThisYear) * 100);
+	return ($percentagediff);
+}
+
+function cal_days_in_year($year) {
+	$days=0; 
+	for($month=1;$month<=12;$month++){
+		$days = $days + cal_days_in_month(CAL_GREGORIAN,$month,$year);
+	}
+	
+	return $days;
+}
 ?>
+
