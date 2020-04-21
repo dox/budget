c2d1cf161286e7504331816feb6515be52ba741e
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-04-21 10:40:43 +0100

    Tidying up sample config
    Functions are now in global_functions.php

--- inc/config.SAMPLE.php
+++ inc/config.SAMPLE.php
@@ -14,29 +14,5 @@ define("LDAP_ACCOUNT_SUFFIX", "@some.ldap.account.suffix");
 
 define("BUDGET_STARTDATE", "2019-08-01");
 define("BUDGET_ENDDATE", "2020-07-31");
-
-function daysIntoBudget() {
-	$timediff = time() - strtotime(BUDGET_STARTDATE);
-	$datediff = round($timediff / (60 * 60 * 24));
-	
-	return ($datediff);
-}
-
-function percentageIntoBudget() {
-	$totalDaysThisYear = cal_days_in_year(date('Y'));
-	$daysIntoBudget = daysIntoBudget();
-	
-	$percentagediff = round(($daysIntoBudget / $totalDaysThisYear) * 100);
-	return ($percentagediff);
-}
-
-function cal_days_in_year($year) {
-	$days=0; 
-	for($month=1;$month<=12;$month++){
-		$days = $days + cal_days_in_month(CAL_GREGORIAN,$month,$year);
-	}
-	
-	return $days;
-}
 ?>
 
