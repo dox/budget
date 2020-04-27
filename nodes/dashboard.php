697660f84586e68b4204159f7f334d5ba0905b67
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-04-27 19:56:32 +0100

    Card width tidy up

--- nodes/dashboard.php
+++ nodes/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_GET['month'])) {
 	$dateReference = $_GET['month'];
-	
+
 } else {
 	$dateReference = date('Y-m-d');
 }
@@ -21,7 +21,7 @@ foreach ($ordersAll AS $order) {
 	} else {
 		$YTDTotalSpend = $YTDTotalSpend + $order['value'];
 	}
-	
+
 	if (empty($order['paid'])) {
 		$outstandingPayments = $outstandingPayments + $order['value'];
 	}
@@ -39,7 +39,7 @@ foreach ($ordersThisMonth AS $order) {
 
 if (empty($monthlyOrdersTotalArray)){
 	$monthlyOrdersTotalArray = array();
-	
+
 }
 $totalSpendMonthly = array_sum($monthlyOrdersTotalArray);
 ?>
@@ -49,25 +49,23 @@ $totalSpendMonthly = array_sum($monthlyOrdersTotalArray);
 
 <canvas id="myChart" width="400" height="100"></canvas>
 <br />
-<div class="container">
-	<div class="row">
-		<div class="col-sm">
-			<div class="card card--blue">
-				<h2 style="font-size: 20px;">&pound; <?php echo number_format($totalSpendMonthly);?></h2>
-			<div class="mt-1" style="color: #A7AEBB;">Outgoings This Month</div>
+<div class="row">
+	<div class="col-sm">
+		<div class="card card--blue">
+			<h2 style="font-size: 20px;">&pound; <?php echo number_format($totalSpendMonthly);?></h2>
+		<div class="mt-1" style="color: #A7AEBB;">Outgoings This Month</div>
+	</div>
+	</div>
+	<div class="col-sm">
+		<div class="card card--red">
+			<h2 style="font-size: 20px;">&pound; <?php echo number_format($YTDTotalSpend);?></h2>
+			<div class="mt-1" style="color: #A7AEBB;">Outgoings This Budget Year</div>
 		</div>
-		</div>
-		<div class="col-sm">
-			<div class="card card--red">
-				<h2 style="font-size: 20px;">&pound; <?php echo number_format($YTDTotalSpend);?></h2>
-				<div class="mt-1" style="color: #A7AEBB;">Outgoings This Budget Year</div>
-			</div>
-		</div>
-		<div class="col-sm">
-			<div class="card card--green">
-				<h2 style="font-size: 20px;">&pound; <?php echo number_format($outstandingPayments);?></h2>
-				<div class="mt-1" style="color: #A7AEBB;">Unpaid Orders</div>
-			</div>
+	</div>
+	<div class="col-sm">
+		<div class="card card--green">
+			<h2 style="font-size: 20px;">&pound; <?php echo number_format($outstandingPayments);?></h2>
+			<div class="mt-1" style="color: #A7AEBB;">Unpaid Orders</div>
 		</div>
 	</div>
 </div>
@@ -79,7 +77,7 @@ $totalSpendMonthly = array_sum($monthlyOrdersTotalArray);
 			<th scope="col" style="width: 50px;"></th>
 			<th scope="col" style="width: 120px;">Code</th>
 			<th scope="col">Name</th>
-			<th scope="col" style="width: 100px;">Value</th>
+			<th scope="col" style="width: 110px;">Value</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -87,22 +85,22 @@ $totalSpendMonthly = array_sum($monthlyOrdersTotalArray);
 		foreach ($monthlyOrdersTotalArray AS $ordersTotal => $value) {
 			$cost_centre_class = new class_cost_centres;
 			$cost_centre = $cost_centre_class->getOne($ordersTotal);
-			
+
 			$output  = "<tr>";
 			$output .= "<td scope=\"row\" style=\"width: 50px;\"><div style=\"width: 15px; height: 15px; border-radius: 2px; background: " . $cost_centre['colour'] . ";\"></div></td>";
 			$output .= "<td><a href=\"index.php?n=costcentres_unique&uid=" . $cost_centre['uid'] . "\">" . $cost_centre['code'] . "</td>";
 			$output .= "<td>" . $cost_centre['name'] . "</td>";
 			$output .= "<td class=\"text-right color-red\">£" . number_format($value) . " <i class=\"fas fa-long-arrow-alt-right fa-sm\"></i></td>";
 
-			
+
 			$output .= "</tr>";
-			
+
 			echo $output;
-			
-			
+
+
 		}
 		?>
-		
+
 	</tbody>
 </table>
 
@@ -112,7 +110,7 @@ $totalSpendMonthly = array_sum($monthlyOrdersTotalArray);
 	$date = date('Y-m-d');
 	do {
 		$ordersThisMonth2 = $orders_class->allByMonth($date);
-		
+
 		foreach ($ordersThisMonth2 AS $orders) {
 			$arrayKeyValue = "'" . date('M',strtotime($orders['date'])) . "'";
 			if (empty($totalOrdersByMonthArray[$arrayKeyValue])) {
