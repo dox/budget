0af7ba15f7b6e7e09ed03f1a25b9360c310c2b6f
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-04-28 17:03:04 +0100

    Ordering cost centres by name asccending

--- inc/cost_centres.php
+++ inc/cost_centres.php
@@ -5,59 +5,59 @@ public $meterUID;
 
 public function groups() {
 	global $db;
-	
+
 	//$groups = $db->where("department", $_SESSION['department']);
 	$groups = $db->where("department", $_SESSION['department']);
 	$groups = $db->groupBy("grouping");
 	$groups = $db->get("cost_centres", null, "grouping");
-	
+
 	foreach ($groups AS $group) {
 		$returnArray[] = $group['grouping'];
 	}
-	
+
 	return $returnArray;
 }
 
 public function totalSpendByCostCentre($uid = null) {
 	global $db;
-	
+
 	$totalSpend = $db->where("cost_centre", $uid);
 	$totalSpend = $db->where('date', Array (BUDGET_STARTDATE, BUDGET_ENDDATE), 'BETWEEN');
 	$totalSpend = $db->getOne ("orders", "sum(value) AS value");
-	
+
 	return $totalSpend['value'];
 }
 
 public function getOne($uid = null) {
 	global $db;
-	
+
 	$meter = $db->where("uid", $uid);
 	$meter = $db->where("department", $_SESSION['department']);
 	$meter = $db->getOne("cost_centres");
-	
+
 	return $meter;
 }
 
 public function all() {
 	global $db;
-	
+
 	$costcentres = $db->where("department", $_SESSION['department']);
 	$costcentres = $db->orderBy('grouping', "ASC");
-	$costcentres = $db->orderBy('name', "DESC");
+	$costcentres = $db->orderBy('name', "ASC");
 	$costcentres = $db->get("cost_centres");
-	
+
 	return $costcentres;
 }
 
 public function insert($data = null) {
 	global $db;
-	
+
 	//$data = $db->escape ($data);
 	$id = $db->escape($db->insert('cost_centres', $data));
-	
+
 	$log = new class_logs;
 	$log->insert("create", $db->getLastQuery());
-	
+
 	if (!$id) {
 		echo 'Log failed: ' . $db->getLastError();
 	}
@@ -65,13 +65,13 @@ public function insert($data = null) {
 
 public function update($uid = null, $data = null) {
 	global $db;
-	
+
 	$db->where ('uid', $uid);
 	$id = $db->escape($db->update('cost_centres', $data));
-	
+
 	$log = new class_logs;
 	$log->insert("update", $db->getLastQuery());
-	
+
 	if (!$id) {
 		echo 'Log failed: ' . $db->getLastError();
 	}
