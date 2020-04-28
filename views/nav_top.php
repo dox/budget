d5b4b44366f43ffa4623275b1774586d83a93f80
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-04-28 13:29:35 +0100

    Navbar now responds better on mobile/small

--- views/nav_top.php
+++ views/nav_top.php
@@ -1,15 +1,12 @@
-<div class="container">
-	<nav class="navbar navbar-expand navbar-light">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+<nav class="navbar navbar-expand navbar-light">
+	<div class="container">
 
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
 			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active"><a class="nav-link" href="index.php?n=dashboard"><i class="far fa-home fa-sm color-blue"></i> Dashboard</a></li>
-				<li class="nav-item"><a class="nav-link" href="index.php?n=orders_all"><i class="far fa-exchange-alt fa-sm color-green"></i> Orders</a></li>
-				<li class="nav-item"><a class="nav-link" href="index.php?n=costcentres_all"><i class="far fa-coins fa-sm color-red"></i> Cost Centres</a></li>
-				<li class="nav-item"><a class="nav-link" href="index.php?n=reports_all"><i class="far fa-chart-line fa-sm color-blue"></i> Reports</a></li>
+				<li class="nav-item active"><a class="nav-link" href="index.php?n=dashboard"><span class="d-none d-sm-block"><i class="far fa-home fa-sm color-blue"></i> Dashboard</span><span class="d-block d-sm-none"><i class="far fa-home fa-lg color-blue"></i></span></a></li>
+				<li class="nav-item"><a class="nav-link" href="index.php?n=orders_all"><span class="d-none d-sm-block"><i class="far fa-exchange-alt fa-sm color-green"></i> Orders</span><span class="d-block d-sm-none"><i class="far fa-exchange-alt fa-lg color-green"></i></span></a></li>
+				<li class="nav-item"><a class="nav-link" href="index.php?n=costcentres_all"><span class="d-none d-sm-block"><i class="far fa-coins fa-sm color-red"></i> Cost Centres</span><span class="d-block d-sm-none"><i class="far fa-coins fa-lg color-red"></i></span></a></li>
+				<li class="nav-item"><a class="nav-link" href="index.php?n=reports_all"><span class="d-none d-sm-block"><i class="far fa-chart-line fa-sm color-blue"></i> Reports</span><span class="d-block d-sm-none"><i class="far fa-chart-line fa-lg color-blue"></i></span></a></li>
 			</ul>
 			<div class="btn-group btn-group-sm float-right">
 				<button onclick="window.location.href = 'index.php?n=orders_create';" type="button" class="btn btn-primary">Create Order</button>
@@ -26,6 +23,6 @@
 			<!--<form class="form-inline my-2 my-lg-0">
 				<input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
 			</form>-->
-		</div>
-	</nav>
-</div>
+
+	</div>
+</nav>
