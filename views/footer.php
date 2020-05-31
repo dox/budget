9b933dc53402a0c4246e4494e0729e30b1f09f55
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-05-31 13:55:31 +0100

    Correct link to github repo

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 <footer class="text-muted">
 	<div class="container">
 		<p class="float-right"><a href="#">Back to top</a></p>
-		<p>SEH Budget  is &copy; <a href="https://github.com/dox">Andrew Breakspear</a>, but please download and customise it for yourself.</p>
+		<p>SEH Budget  is &copy; <a href="https://github.com/dox/budget">Andrew Breakspear</a>, but please download and customise it for yourself.</p>
 	</div>
 </footer>
