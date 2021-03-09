8dadd2548fb0cb80122c9809b7f4aa9fe3fe0046
Andrew Breakspear <andrew@longhanborough.com>
2021-03-09 09:00:38 +0000

    favicons

--- index.php
+++ index.php
@@ -86,12 +86,23 @@ if (isset($_POST['loginformsubmit'])) { //prevent null bind
 	<link rel="stylesheet" href="css/app.css"/>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.css" integrity="sha256-IvM9nJf/b5l2RoebiFno92E5ONttVyaEEsdemDC6iQA=" crossorigin="anonymous" />
 
-	<link rel="apple-touch-icon" sizes="57x57" href="ico/apple-touch-icon-57-precomposed.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-  <link rel="icon" type="image/png" href="ico/favicon.png">
-  <link rel="shortcut icon" href="ico/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" sizes="57x57" href="/ico/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/ico/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/ico/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/ico/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/ico/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/ico/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/ico/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/ico/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/ico/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/ico/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/ico/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/ico/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/ico/favicon-16x16.png">
+	<link rel="manifest" href="/ico/manifest.json">
+	<meta name="msapplication-TileColor" content="#f8f9fb">
+	<meta name="msapplication-TileImage" content="/ico/ms-icon-144x144.png">
+	<meta name="theme-color" content="#f8f9fb">
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js" integrity="sha256-TQq84xX6vkwR0Qs1qH5ADkP+MvH0W+9E7TdHJsoIQiM=" crossorigin="anonymous"></script>
