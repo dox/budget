6d65a13ed217154110234a2b9e975f951e97e5ba
Andrew Breakspear <andrew@longhanborough.com>
2021-03-22 13:19:19 +0000

    Catching null values

--- nodes/suppliers_edit.php
+++ nodes/suppliers_edit.php
@@ -10,31 +10,31 @@ $supplierURL = "index.php?n=suppliers_unique&name=" . urlencode($_GET['name']);
 <form method="POST" action="<?php echo $supplierURL; ?>">
 	<div class="mb-3">
 		<label for="name">Name</label>
-		<input type="text" class="form-control" readonly id="name" name="name" placeholder="Name" value="<?php echo $_GET['name']; ?>">
+		<input type="text" class="form-control" readonly id="name" name="name" placeholder="Name" value="<?php if (isset($_GET['name'])) { echo $_GET['name'];} ?>">
 	</div>
 	<div class="mb-3">
 		<label for="account_number">Account #</label>
-		<input type="text" class="form-control" id="account_number" name="account_number" placeholder="Account #" value="<?php echo $supplier['account_number']; ?>">
+		<input type="text" class="form-control" id="account_number" name="account_number" placeholder="Account #" value="<?php if (isset($supplier['account_number'])) { echo $supplier['account_number'];} ?>">
 	</div>
 	<div class="mb-3">
 		<label for="address">Address</label>
-		<textarea class="form-control" id="address" name="address" rows="3"><?php echo $supplier['address'];?></textarea>
+		<textarea class="form-control" id="address" name="address" rows="3"><?php if (isset($supplier['address'])) { echo $supplier['address'];} ?></textarea>
 	</div>
 	<div class="mb-3">
 		<label for="telephone">Telephone</label>
-		<input type="text" class="form-control" id="telephone" name="telephone" placeholder="Telephone" value="<?php echo $supplier['telephone']; ?>">
+		<input type="text" class="form-control" id="telephone" name="telephone" placeholder="Telephone" value="<?php if (isset($supplier['telephone'])) { echo $supplier['telephone'];} ?>">
 	</div>
 	<div class="mb-3">
 		<label for="mobile">Mobile</label>
-		<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile" value="<?php echo $supplier['mobile']; ?>">
+		<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile" value="<?php if (isset($supplier['mobile'])) { echo $supplier['mobile'];} ?>">
 	</div>
 	<div class="mb-3">
 		<label for="email">Email</label>
-		<input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?php echo $supplier['email']; ?>">
+		<input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?php if (isset($supplier['email'])) { echo $supplier['email'];} ?>">
 	</div>
 	<div class="mb-3">
 		<label for="website">Website</label>
-		<input type="text" class="form-control" id="website" name="website" placeholder="www.website.com" value="<?php echo $supplier['website']; ?>">
+		<input type="text" class="form-control" id="website" name="website" placeholder="www.website.com" value="<?php if (isset($supplier['website'])) { echo $supplier['website'];} ?>">
 	</div>
 	<button type="submit" class="btn btn-primary">Update</button>
 </form>
