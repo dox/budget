a1935b16c854a2e3c24279c20194652b3968b7c9
Andrew Breakspear <andrew@longhanborough.com>
2021-04-13 20:06:18 +0100

    Simplifying export

--- export.php
+++ export.php
@@ -1,42 +1,54 @@
 <?php
 require_once('inc/autoload.php');
 
-if (isset($_SESSION['username'])) {
-  header('Content-Type: text/csv');
-  header('Content-Disposition: attachment; filename="export.csv"');
-  header('Pragma: no-cache');
-  header('Expires: 0');
-  echo exportToCsv("orders");
+$sql  = "SELECT * FROM orders ";
+$sql .= "WHERE cost_centre in (SELECT uid FROM cost_centres WHERE department = '" . $_SESSION['department'] . "') ";
+$sql .= "ORDER BY orders.date DESC, orders.po DESC;";
+
+$orders = $db->rawQuery($sql);
+
+$i = 0;
+foreach ($orders AS $order) {
+  $cost_centre_class = new class_cost_centres;
+  $cost_centre = $cost_centre_class->getOne($order['cost_centre']);
+
+  $users_class = new class_users;
+  $user = $users_class->getOne($order['username']);
+
+  $row['uid']         = $order['uid'];
+  $row['username']    = $user['username'];
+  $row['date']        = $order['date'];
+
+  $row['cost_centre_code'] = $cost_centre['code'];
+  $row['cost_centre_name'] = $cost_centre['name'];
+
+  $row['po']          = $order['po'];
+  $row['order_num']   = $order['order_num'];
+  $row['name']        = $order['name'];
+  $row['value']       = $order['value'];
+  $row['supplier']    = $order['supplier'];
+  $row['description'] = $order['description'];
+  $row['paid']        = $order['paid'];
+
+  $prod[$i] = $row;
+  $i++;
 }
 
+$output = fopen("php://output",'w') or die("Can't open php://output");
+header("Content-Type:application/csv");
+header("Content-Disposition:attachment;filename=budget_export.csv");
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="budget_export.csv"');
+header('Pragma: no-cache');
+header('Expires: 0');
 
-function exportToCsv($type) {
-  global $db;
+fputcsv($output, array_keys($row));
 
-  if ($type == "orders") {
-    $sql  = "SELECT * FROM orders ";
-    $sql .= "WHERE cost_centre in (SELECT uid FROM cost_centres WHERE department = '" . $_SESSION['department'] . "') ";
-    $sql .= "ORDER BY orders.date DESC, orders.po DESC;";
-
-    $dbRows = $db->rawQuery($sql);
-
-    $arrayKeys = array_keys($dbRows[0]);
+if (isset($_SESSION['username'])) {
+  foreach($prod as $product) {
+      fputcsv($output, $product);
   }
 
-  $output = implode(",", $arrayKeys) . "\n";
-  foreach ($dbRows AS $dbRow) {
-
-    $rowOutput = array();
-    foreach ($dbRow AS $column) {
-      $column = str_replace(",", "", $column);
-      $column = str_replace('"', '"', $column);
-      $column = str_replace("\r", "", $column);
-
-      $rowOutput[] = str_replace(",", "", $column);
-    }
-    $output .= implode(",", $rowOutput) . "\n";
-  }
-
-  return $output;
+  fclose($output) or die("Can't close php://output");
 }
 ?>
